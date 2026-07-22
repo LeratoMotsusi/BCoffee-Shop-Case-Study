fcc2c573ae3bea5671dda7d325d46ebedfab86fc
lerato@brightlearn.co.za <lerato@brightlearn.co.za>
2026-07-22 18:00:59 +0000

    Coding continued

--- BCoffe Shop Coding.sql
+++ BCoffe Shop Coding.sql
@@ -9,6 +9,7 @@ SELECT COUNT(*) AS Num_of_rows,
        COUNT(DISTINCT transaction_id) AS user_id
 FROM bright.coffee.shop;
 
+
 ---3. Checking for Nulls in some of the columns of the dataset
 SELECT COUNT(*) AS NULL_ROWS
 FROM bright.coffee.shop
@@ -24,9 +25,57 @@ SELECT MIN(transaction_date) AS earliest_date,
 FROM bright.coffee.shop;
 
 
-----Calculating Revenue
+----5.Calculating Revenue
 SELECT SUM(transaction_qty * unit_price) AS Total_Revenue
 FROM bright.coffee.shop;
+
+---6.Calculating Revenue by changing the data type of the unit_price column to be Double and to have dots instead of commas
+SELECT ROUND(SUM(transaction_qty * TRY_CAST(REPLACE(unit_price, ',','.') AS DOUBLE)),2) AS Total_Revenue
+FROM bright.coffee.shop;
+
+---7.Checking the distinct branches
+SELECT DISTINCT store_location,
+                store_id
+FROM bright.coffee.shop;
+
+
+---7.Checking the distinct product categories
+SELECT DISTINCT product_category
+FROM bright.coffee.shop;
+
+---8.Checking the distinct product types
+SELECT DISTINCT product_type
+FROM bright.coffee.shop;
+
+---9. Checking the lowest and highest prices in the unit_price column
+SELECT MIN(TRY_CAST(REPLACE(unit_price, ',','.') AS DOUBLE)) AS lowest_unit_price,
+       MAX(TRY_CAST(REPLACE(unit_price, ',','.') AS DOUBLE)) AS highest_unit_price
+FROM bright.coffee.shop;
+
+
+---10. Checking the Total Revenue by store location
+SELECT DISTINCT store_location,
+        ROUND(SUM(transaction_qty * TRY_CAST(REPLACE(unit_price, ',','.') AS DOUBLE)),2) AS Total_Revenue
+FROM bright.coffee.shop
+GROUP BY store_location
+ORDER BY Total_Revenue;
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
